5798fa8b63fe6ece76f7c74d0ea75a8f9db140d2
Samad Arshad <samad.arshad@ev.energy>
2024-05-18 22:09:24 +0100

    Example images

--- src/components/trackers/tracker.tsx
+++ src/components/trackers/tracker.tsx
@@ -1,24 +1,34 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart, faEdit } from "@fortawesome/free-regular-svg-icons";
 import TrackerChartExample from "@/components/trackers/tracker-chart-example";
-import { Skeleton } from "@nextui-org/react";
+import { Skeleton, Image, Avatar } from "@nextui-org/react";
 
 export function Tracker() {
   return (
     <div className="border rounded p-2">
       <div className="flex flex-row justify-between">
-        <div className="flex flex-row gap-2">
-          <p>Logo</p>
-          <h3 className="text-lg font-bold">Tracker Title</h3>
+        <div className="flex flex-row gap-4 items-center">
+          <Image
+            src="/logo.png"
+            alt="logo"
+            height={40}
+            width={40}
+            radius="none"
+          />
+          <h3 className="text-lg font-bold">Udacity</h3>
         </div>
-        <div className="flex flex-row gap-4">
-          <FontAwesomeIcon size="2x" icon={faHeart} />
-          <FontAwesomeIcon size="2x" icon={faEdit} />
+        <div className="flex flex-row gap-4 items-center">
+          <FontAwesomeIcon size="1x" icon={faHeart} />
+          <FontAwesomeIcon size="1x" icon={faEdit} />
         </div>
       </div>
-      <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="grid grid-cols-4 p-4">
         <div className="col-span-1">
-          <Skeleton disableAnimation={true} className="w-full h-52" />
+          <Image
+            src="/webpage-preview.png"
+            alt="webpage preview"
+            radius="none"
+          />
         </div>
         <div className="col-span-3">
           <TrackerChartExample />
