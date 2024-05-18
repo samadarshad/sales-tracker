45c5018eab52bcba2b40e178aaee8bc9bf625366
Samad Arshad <samad.arshad@ev.energy>
2024-05-18 13:05:02 +0100

    Header initial

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,33 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+  Input,
+} from "@nextui-org/react";
+
 export default function Header() {
-  return <div>Header</div>
+  return (
+    <Navbar>
+      <NavbarBrand>
+        <Link href="/" className="font-bold">
+          Sales Tracker
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem>
+          <Input name="search" label="Search" />
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Button color="primary" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
 }
