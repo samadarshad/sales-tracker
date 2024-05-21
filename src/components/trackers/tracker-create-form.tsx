8a4b81ae8ca127b5a21d5c8e41fb90c2d45854bf
Samad Arshad <samad.arshad@ev.energy>
2024-05-21 07:28:33 +0100

    Slide image into view

--- src/components/trackers/tracker-create-form.tsx
+++ src/components/trackers/tracker-create-form.tsx
@@ -13,29 +13,41 @@ import {
   AccordionItem,
   Textarea,
   Spinner,
-  Skeleton,
 } from "@nextui-org/react";
 import * as actions from "@/actions";
 import { useFormState } from "react-dom";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 export default function TrackerCreateForm() {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const [formState, action] = useFormState(actions.setWebsiteUrl, {
     errors: {},
   });
+  const [loading, setLoading] = useState(false);
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const [collapseImage, setCollapseImage] = useState(false);
+
   const verifyWebsite = async (formData: FormData) => {
     setLoading(true);
+    setImageLoaded(false);
+    setCollapseImage(false);
     await action(formData);
   };
+
   const clearWebsite = async () => {
     setLoading(false);
+    setImageLoaded(false);
+    setCollapseImage(false);
     await action(new FormData());
   };
-  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     setLoading(false);
+    setImageLoaded(false);
   }, [formState]);
 
   // use form state, with the url to the image as the return, and any errors
@@ -56,48 +68,60 @@ export default function TrackerCreateForm() {
                     name="website-url"
                     label="Website URL"
                     placeholder="e.g. www.udacity.com"
-                    onBlur={async (e) => {
-                      const formData = new FormData();
-                      formData.append(
-                        "website-url",
-                        (e.target as HTMLInputElement).value
-                      );
-                      await verifyWebsite(formData);
-                    }}
-                    // endContent={loading ? <Spinner /> : null}
-                    isClearable
-                    onClear={clearWebsite}
+                    className="flex items-center justify-center"
+                    endContent={
+                      loading ? (
+                        <Spinner />
+                      ) : imageLoaded && collapseImage ? null : (
+                        <IconButton
+                          type="button"
+                          sx={{ p: "10px" }}
+                          aria-label="search"
+                          className="flex items-center justify-center"
+                          onClick={async (e) => {
+                            const formData = new FormData();
+                            formData.append(
+                              "website-url",
+                              (e.target as HTMLInputElement).value
+                            );
+                            await verifyWebsite(formData);
+                          }}
+                        >
+                          <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </IconButton>
+                      )
+                    }
+                    isClearable={imageLoaded && collapseImage}
+                    onClear={
+                      imageLoaded && collapseImage ? clearWebsite : undefined
+                    }
                   />
                 </form>
-                <div className="grid grid-cols-3 gap-4">
-                  <div className="col-span-1">
-                    <h3 className="font-bold">Website Preview</h3>
-                    <p className="text-gray-600 font-thin">
-                      Ensure the sale is clearly visible in the preview for the
-                      AI to detect it
-                    </p>
+                <Collapse in={imageLoaded && collapseImage}>
+                  <div className="grid grid-cols-3 gap-4">
+                    <div className="col-span-1">
+                      <h3 className="font-bold">Website Preview</h3>
+                      <p className="text-gray-600 font-thin">
+                        Ensure the sale is clearly visible in the preview for
+                        the AI to detect it
+                      </p>
+                    </div>
+                    <div className="col-span-2">
+                      {formState.previewUrl ? (
+                        <Image
+                          src={formState.previewUrl}
+                          alt="logo"
+                          width={1000}
+                          height={160}
+                          onLoad={() => {
+                            setImageLoaded(true);
+                            setCollapseImage(true);
+                          }}
+                        />
+                      ) : null}
+                    </div>
                   </div>
-                  <div className="col-span-2">
-                    {formState.previewUrl ? (
-                      <Image
-                        src={formState.previewUrl || ""}
-                        alt="logo"
-                        width={1000}
-                        height={160}
-                      />
-                    ) : (
-                      <div>
-                        {loading ? (
-                          <Skeleton>
-                            <div className="w-full h-40"></div>
-                          </Skeleton>
-                        ) : (
-                          <div className="flex w-full h-40 bg-gray-200 justify-center items-center"></div>
-                        )}
-                      </div>
-                    )}
-                  </div>
-                </div>
+                </Collapse>
                 <Accordion>
                   <AccordionItem
                     title={<h3 className="font-thin">(Advanced) AI Prompt</h3>}
