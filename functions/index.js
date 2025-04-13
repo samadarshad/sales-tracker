d794fd67e9d114164c2eeb55b444df2481bcdb2e
Samad Arshad <samad.arshad@ev.energy>
2025-04-13 18:37:45 +0100

    es6

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,17 @@
 /**
  * Import function triggers from their respective submodules:
  *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ * import {onCall} from "firebase-functions/v2/https"; // Example ES6 import
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore"; // Example ES6 import
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+// Use import instead of require
+import {onRequest} from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
+import * as functions from "firebase-functions";
+import admin from "firebase-admin"; // Use default import for admin
 
 admin.initializeApp();
 const db = admin.firestore();
@@ -19,12 +19,14 @@ const db = admin.firestore();
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.helloWorld = onRequest((request, response) => {
+// Use export const instead of exports.
+export const helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
 
-exports.dailyTrackerProcessor = functions.https.onRequest(async (request, response) => {
+// Use export const instead of exports.
+export const dailyTrackerProcessor = functions.https.onRequest(async (request, response) => {
     console.log("Starting daily tracker processing job on request.");
 
     const trackersRef = db.collection("trackers");
